bf2e974bba83877897ca431370324f231126bfc5
noracato <ncschinkel@gmail.com>
2025-02-28 13:03:17 +0100

    Redirect all slugs

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,21 @@ const nextConfig: NextConfig = {
         destination: 'https://collections.energytransitionmodel.com/:slug',
         permanent: true,
       },
+      {
+        source: '/:slug/:charts',
+        destination: 'https://collections.energytransitionmodel.com/:slug/:charts',
+        permanent: true,
+      },
+      {
+        source: '/:slug/:charts/:chart',
+        destination: 'https://collections.energytransitionmodel.com/:slug/:charts/:chart',
+        permanent: true,
+      },
+      {
+        source: '/:slug/:charts/:chart/:sub',
+        destination: 'https://collections.energytransitionmodel.com/:slug/:charts/:chart/:sub',
+        permanent: true,
+      },
     ]
   },
 };
